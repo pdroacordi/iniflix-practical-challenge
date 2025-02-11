cc7611f052b2f19adbbae73f1e1a6d9ef701b21c
Pedro Acordi <pedroacordi2@gmail.com>
2025-02-11 18:35:32 -0300

    feat: adiciona formatador de salario e data

--- iniflix-challenge/src/utils/Formatador.java
+++ iniflix-challenge/src/utils/Formatador.java
@@ -1,4 +1,21 @@
 package utils;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Formatador {
+
+    public static String formatar(BigDecimal valor) {
+        NumberFormat nf = NumberFormat.getCurrencyInstance( new Locale("pt", "BR") );
+        return nf.format(valor);
+    }
+
+    public static String formatar(LocalDate valor) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        return dtf.format(valor);
+    }
 }
