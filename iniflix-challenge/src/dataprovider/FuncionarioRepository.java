00426eb050b055d3e54bf7b38892709c2d40a5d5
Pedro Acordi <pedroacordi2@gmail.com>
2025-02-11 18:25:55 -0300

    feat: adiciona repositório de funcionários

--- iniflix-challenge/src/dataprovider/FuncionarioRepository.java
+++ iniflix-challenge/src/dataprovider/FuncionarioRepository.java
@@ -3,10 +3,46 @@ package dataprovider;
 import core.domain.Funcionario;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class ListaFuncionarios {
+public class FuncionarioRepository {
 
-    public static final List<Funcionario> FUNCIONARIOS = new ArrayList<Funcionario>();
+    private static final List<Funcionario> FUNCIONARIOS = new ArrayList<Funcionario>();
+
+    public static boolean adicionarFuncionario(Funcionario funcionario) {
+        FUNCIONARIOS.add(funcionario);
+        return true;
+    }
+
+    public static boolean adicionarFuncionarios(List<Funcionario> funcionarios) {
+        FUNCIONARIOS.addAll(funcionarios);
+        return true;
+    }
+
+    public static boolean removerFuncionario(String nomeFuncionario) {
+        List<Funcionario> aRemover = new ArrayList<>();
+        for( Funcionario f : FUNCIONARIOS ) {
+            if( f.getNome().equals(nomeFuncionario) ) {
+                aRemover.add(f);
+            }
+        }
+        if( aRemover.isEmpty() ) {
+            return false;
+        }
+        FUNCIONARIOS.removeAll(aRemover);
+        return true;
+    }
+
+    public static List<Funcionario> obterFuncionarios() {
+        return FUNCIONARIOS;
+    }
+
+    public static Funcionario obterFuncionarioPorNome(String nomeFuncionario) {
+        for( Funcionario f : FUNCIONARIOS ) {
+            if( f.getNome().equals(nomeFuncionario) ) {
+                return f;
+            }
+        }
+        return null;
+    }
 }
