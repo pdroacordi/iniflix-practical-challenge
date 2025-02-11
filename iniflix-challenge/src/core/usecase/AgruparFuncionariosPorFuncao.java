d6d45f30616391cd1c7c3888d520585a131d8d78
Pedro Acordi <pedroacordi2@gmail.com>
2025-02-11 19:02:25 -0300

    feat: conclui a tarefa 3.5

--- iniflix-challenge/src/core/usecase/AgruparFuncionariosPorFuncao.java
+++ iniflix-challenge/src/core/usecase/AgruparFuncionariosPorFuncao.java
@@ -1,4 +1,21 @@
 package core.usecase;
 
+import core.domain.Funcionario;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class AgruparFuncionariosPorFuncao {
+
+    public static Map<String, List<Funcionario>> obterFuncionariosPorFuncao(List<Funcionario> funcionarios) {
+        Map<String, List<Funcionario>> funcionariosPorFuncao = new HashMap<>();
+        funcionarios.forEach(funcionario -> {
+            String funcao = funcionario.getFuncao();
+            funcionariosPorFuncao.computeIfAbsent(funcao, k -> new ArrayList<>())
+                    .add(funcionario);
+        });
+        return funcionariosPorFuncao;
+    }
 }
